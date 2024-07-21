ef94a1be2df64d27346d514a478de1aa647762d4
jackvdp <jack@pumpymusic.co.uk>
2024-07-21 13:34:17 +0100

    Refactor navbar js

--- src/js/modules/navbar.js
+++ src/js/modules/navbar.js
@@ -2,44 +2,40 @@ const navbar = document.querySelectorAll('.navbar');
 const body = document.body;
 
 navbar.forEach(function (element) {
-    element.addEventListener('hide.bs.collapse', function () {
-        body.classList.remove('navbar-active');
-    })
-    element.addEventListener('show.bs.collapse', function () {
-        setTimeout(() => {
-            body.classList.add('navbar-active');
-        }, 0)
-    })
+  element.addEventListener('hide.bs.collapse', function () {
+    body.classList.remove('navbar-active');
+  })
+  element.addEventListener('show.bs.collapse', function () {
+    setTimeout(() => {
+      body.classList.add('navbar-active');
+    }, 0)
+  })
 })
 
 function updateNavbarTransparency() {
-    const navbars = document.querySelectorAll('.navbar-dynamic-bg');
-    const scrollThreshold = 100; // Adjust this value as needed
-  
-    navbars.forEach(navbar => {
-      const logoDark = navbar.querySelector('.logo-dark');
-      const logoLight = navbar.querySelector('.logo-light');
-  
-      if (window.scrollY <= scrollThreshold && !navbar.classList.contains('navbar-active')) {
-        navbar.classList.add('navbar-transparent');
-        if (logoDark && logoLight) {
-          logoDark.style.display = 'none';
-          logoLight.style.display = 'inline-block';
-        }
-      } else {
-        navbar.classList.remove('navbar-transparent');
-        if (logoDark && logoLight) {
-          logoDark.style.display = 'inline-block';
-          logoLight.style.display = 'none';
-        }
+  const navbars = document.querySelectorAll('.navbar-dynamic-bg');
+  const scrollThreshold = 100; // Adjust this value as needed
+
+  navbars.forEach(navbar => {
+    const logoDark = navbar.querySelector('.logo-dark');
+    const logoLight = navbar.querySelector('.logo-light');
+
+    if (window.scrollY <= scrollThreshold && !navbar.classList.contains('navbar-active')) {
+      navbar.classList.add('navbar-transparent');
+      if (logoDark && logoLight) {
+        logoDark.style.display = 'none';
+        logoLight.style.display = 'inline-block';
       }
-    });
-  }
-  
-  // Call the function on scroll and on page load
-  window.addEventListener('scroll', updateNavbarTransparency);
-  window.addEventListener('load', updateNavbarTransparency);
-  
-  // Call the function on scroll and on page load
-  window.addEventListener('scroll', updateNavbarTransparency);
-  window.addEventListener('load', updateNavbarTransparency);
+    } else {
+      navbar.classList.remove('navbar-transparent');
+      if (logoDark && logoLight) {
+        logoDark.style.display = 'inline-block';
+        logoLight.style.display = 'none';
+      }
+    }
+  });
+}
+
+// Call the function on scroll and on page load
+window.addEventListener('scroll', updateNavbarTransparency);
+window.addEventListener('load', updateNavbarTransparency);
